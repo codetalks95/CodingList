01fe97bc6f366b6e7b53f8eb2e646089c7e1d2c3
CodeTalks <info@codetalksdna.com>
2023-03-05 19:22:20 +0530

    Added Code for TicTac Toe Game

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+        while (true) {
+            playerMove(board);
+            computerMove(board);
+            printBoard(board);
+        }
+//        computerMove(board);
+    }
+
+    private static void computerMove(char[][] board) {
+        Random random = new Random();
+        int numberInBoard = random.nextInt(9) + 1;
+        placeMove(board, String.valueOf(numberInBoard), 'O');
+    }
+
+    private static boolean isValidMove(char[][] board, int numberInBoard, char symbol) {
+        switch (String.valueOf(numberInBoard)) {
+            case "1" -> board[0][0] = symbol;
+            case "2" -> board[0][1] = symbol;
+            case "3" -> board[0][2] = symbol;
+            case "4" -> board[1][0] = symbol;
+            case "5" -> board[1][1] = symbol;
+            case "6" -> board[1][2] = symbol;
+            case "7" -> board[2][0] = symbol;
+            case "8" -> board[2][1] = symbol;
+            case "9" -> board[2][2] = symbol;
+            default -> System.out.println(":(");
+        }
+        return false;
+    }
+
+    private static void playerMove(char[][] board) {
+        System.out.println("Enter the Place for Input");
+        Scanner scanner = new Scanner(System.in);
+        placeMove(board, String.valueOf(scanner.nextInt()), 'X');
+    }
+
+    private static void printBoard(char[][] board) {
+        System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+    }
+
+    public static void placeMove(char[][] board, String position, char symbol) {
+        switch (position) {
+            case "1" -> board[0][0] = symbol;
+            case "2" -> board[0][1] = symbol;
+            case "3" -> board[0][2] = symbol;
+            case "4" -> board[1][0] = symbol;
+            case "5" -> board[1][1] = symbol;
+            case "6" -> board[1][2] = symbol;
+            case "7" -> board[2][0] = symbol;
+            case "8" -> board[2][1] = symbol;
+            case "9" -> board[2][2] = symbol;
+            default -> System.out.println(":(");
+        }
+    }
+}
