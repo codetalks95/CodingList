482c16fff8e77288baaef5401e88263a28c0e0fa
CodeTalks <info@codetalksdna.com>
2023-03-20 22:02:23 +0530

    Added new Classes and Files

---
+++ src/CodingBat/Boolean/MakeBricks.java
@@ -0,0 +1,16 @@
+package CodingBat.Boolean;
+
+public class MakeBricks {
+    public static void main(String[] args) {
+        int smallBrick = 1;
+        int bigBrick = 5;
+        int goal = 13;
+        int smallBrickQty = 3;
+        int bigBrickQty = 2;
+        System.out.println(makeBricks(smallBrick, smallBrickQty, bigBrick, bigBrickQty, goal));
+    }
+
+    private static boolean makeBricks(int smallBrick, int smallBrickQty, int bigBrickQty, int bigBrick, int goal) {
+        return smallBrick * smallBrickQty + bigBrick * bigBrickQty == goal;
+    }
+}
