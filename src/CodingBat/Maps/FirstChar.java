61f594bf7214d7a2ee5eb0392f575accacf4d56d
CodeTalks <info@codetalksdna.com>
2023-03-21 23:28:39 +0530

    Added Logic for First Char.

--- src/CodingBat/Maps/FirstChar.java
+++ src/CodingBat/Maps/FirstChar.java
@@ -5,17 +5,33 @@ import java.util.Map;
 
 public class FirstChar {
     public static void main(String[] args) {
-        String[] str = {"salt", "tea", "soda", "toast"};
-        System.out.println("HashMap Results are" + " " + pairs(str));
+        System.out.println("HashMap Results are" + " " + pairsMap(new String[]{"aa", "bb", "cc", "aAA", "cCC", "d"}));
+        System.out.println("Concat Map Results are" + " " + concatCharacterMap(new String[]{"aa"}));
     }
 
-    public static Map<String, String> pairs(String[] strings) {
+    public static Map<String, String> pairsMap(String[] strings) {
         Map<String, String> integerMap = new HashMap<>();
+        String str, val;
         for (int i = 0; i < strings.length; i++) {
             for (int j = i + 1; j < strings.length; j++) {
+                String[] stringsData = new String[1];
+                stringsData[0] = strings[i];
+                str = strings[i] + strings[j];
                 if (strings[i].charAt(0) == strings[j].charAt(0)) {
-                    integerMap.put(String.valueOf(strings[i].charAt(0)), strings[i]+strings[j]);
+                    char ch = strings[i].charAt(0);
+                    val = concatCharacterMap(stringsData).get(Character.toString(ch));
+                    integerMap.put(String.valueOf(strings[i].charAt(0)), val + str);
                 }
+            }
+        }
+        return integerMap;
+    }
+
+    public static Map<String, String> concatCharacterMap(String[] strings) {
+        Map<String, String> integerMap = new HashMap<>();
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].charAt(i) == strings[i].charAt(i + 1)) {
+                integerMap.put(String.valueOf(strings[i].charAt(0)), String.valueOf(strings[i].charAt(0)));
             }
         }
         return integerMap;
