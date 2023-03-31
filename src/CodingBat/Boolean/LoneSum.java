3c8ce4abdb10ab7bd8816b2f13c378722138a16d
CodeTalks <info@codetalksdna.com>
2023-03-31 20:37:34 +0530

    Added Two Class logic

--- src/CodingBat/Boolean/LoneSum.java
+++ src/CodingBat/Boolean/LoneSum.java
@@ -13,7 +13,18 @@ public class LoneSum {
         List<Integer> list = Arrays.stream(num).boxed().collect(Collectors.toList());
         List<Integer> integerList = new ArrayList<>();
         System.out.println("List is::" + " " + list);
+        logicComputation(num, list, integerList);
         int number;
+        System.out.println(list);
+        number = getNumber(list);
+        return number;
+    }
+
+    private static void logicComputation(int[] num, List<Integer> list, List<Integer> integerList) {
+        logiCalculate(num, list, integerList);
+    }
+
+    private static void logiCalculate(int[] num, List<Integer> list, List<Integer> integerList) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
                 if (num[i] == num[j]) {
@@ -21,6 +32,10 @@ public class LoneSum {
                 }
             }
         }
+        logic(list, integerList);
+    }
+
+    private static void logic(List<Integer> list, List<Integer> integerList) {
         if (integerList.size() != 0 && !allSameLogic(integerList)) {
             for (Integer integer : integerList) {
                 list.remove(integer);
@@ -28,9 +43,6 @@ public class LoneSum {
         } else {
             list.removeAll(integerList);
         }
-        System.out.println(list);
-        number = getNumber(list);
-        return number;
     }
 
     private static boolean allSameLogic(List<Integer> list) {
