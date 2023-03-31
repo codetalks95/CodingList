42da81c18a22cfcd76066096fb31fc4cd6690bf0
CodeTalks <info@codetalksdna.com>
2023-03-31 22:29:09 +0530

    Added Two Class logic

--- src/CodingBat/Boolean/LoneSum.java
+++ src/CodingBat/Boolean/LoneSum.java
@@ -21,10 +21,10 @@ public class LoneSum {
     }
 
     private static void logicComputation(int[] num, List<Integer> list, List<Integer> integerList) {
-        logiCalculate(num, list, integerList);
+        logicCalculate(num, list, integerList);
     }
 
-    private static void logiCalculate(int[] num, List<Integer> list, List<Integer> integerList) {
+    private static void logicCalculate(int[] num, List<Integer> list, List<Integer> integerList) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
                 if (num[i] == num[j]) {
