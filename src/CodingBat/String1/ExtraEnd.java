34ed83a0924e56e1e49199799b04b3cf85a3a352
CodeTalks <info@codetalksdna.com>
2023-04-01 20:57:54 +0530

    Added new methods and class

--- src/CodingBat/String1/ExtraEnd.java
+++ src/CodingBat/String1/ExtraEnd.java
@@ -2,6 +2,19 @@ package CodingBat.String1;
 
 public class ExtraEnd {
     public static void main(String[] args) {
+        String[] strings = {"Hello"};
+        System.out.println("Extra End Logic result is::" + " " + extraEndLogic(strings));
+    }
 
+    private static StringBuilder extraEndLogic(String[] strings) {
+        String str = null;
+        for (String string : strings) {
+            char[] ch = string.toCharArray();
+            str = String.valueOf(ch[ch.length - 2]) + (ch[ch.length - 1]);
+        }
+        if (str != null) {
+            return new StringBuilder(str.repeat(3));
+        }
+        return null;
     }
 }
