19e3d269cc1b40c6ee7bc46f26d5f791d22ddac9
CodeTalks <info@codetalksdna.com>
2023-04-04 01:39:45 +0530

    Added new classes and methods for Arrays1.

--- src/CodingBat/Arrays1/Double23.java
+++ src/CodingBat/Arrays1/Double23.java
@@ -1,7 +1,18 @@
 package CodingBat.Arrays1;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Double23 {
     public static void main(String[] args) {
-        
+        int[] num = {2, 2, 3, 2, 1, 5, 1};
+        System.out.println("Double23 Logic result is::" + double23Logic(num));
+
+    }
+
+    private static Map<Integer, Long> double23Logic(int[] num) {
+        return Arrays.stream(num).boxed().
+                collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
     }
 }
