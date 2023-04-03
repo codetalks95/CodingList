8f420f19429296a2e4ec76d27a36b36b84633db2
CodeTalks <info@codetalksdna.com>
2023-04-04 01:24:40 +0530

    Added new classes and methods for Arrays1.

--- src/CodingBat/Arrays1/PlusTwo.java
+++ src/CodingBat/Arrays1/PlusTwo.java
@@ -1,7 +1,24 @@
 package CodingBat.Arrays1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlusTwo {
     public static void main(String[] args) {
-        
+        int[][] num = {{1, 2}, {3, 4}};
+        System.out.println("PlusTwo Logic result Logic is::" + plusTwoLogic(num));
+
+    }
+
+    private static List<Integer> plusTwoLogic(int[][] num) {
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 0; i < num[0].length; i++) {
+            integerList.add(num[0][i]);
+        }
+        for (int j = 0; j < num[0].length; j++) {
+            integerList.add(num[1][j]);
+        }
+        return integerList;
     }
 }
+
