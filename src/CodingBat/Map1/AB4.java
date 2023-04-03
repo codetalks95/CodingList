686c441219b7c7d354dbb8f6ef492bcfb4080971
CodeTalks <info@codetalksdna.com>
2023-04-03 19:32:07 +0530

    Added new classes for Map

---
+++ src/CodingBat/Map1/AB4.java
@@ -0,0 +1,35 @@
+package CodingBat.Map1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AB4 {
+    public static void main(String[] args) {
+        Map<String, String> myMap = createMap();
+        System.out.println("Map Produced for ab4Logic is::" + ab4Logic(myMap));
+    }
+
+    private static Map<String, String> ab4Logic(Map<String, String> myMap) {
+        if (myMap.size() >= 3) {
+            if (myMap.get("a").length() != myMap.get("b").length()) {
+                if (myMap.get("a").length() > myMap.get("b").length()) {
+                    myMap.put("c", myMap.get("a"));
+                } else if (myMap.get("a").length() < myMap.get("b").length()) {
+                    myMap.put("c", myMap.get("b"));
+                }
+            } else {
+                myMap.put("a", " ");
+                myMap.put("b"," ");
+            }
+        }
+        return myMap;
+    }
+
+    private static Map<String, String> createMap() {
+        Map<String, String> myMap = new HashMap<String, String>();
+        myMap.put("a", "aaa");
+        myMap.put("b", "bbb");
+        myMap.put("c", "cake");
+        return myMap;
+    }
+}
