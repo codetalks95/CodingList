09b08ce1dfb6a6a55a09356c25754267f514fa2d
CodeTalks <info@codetalksdna.com>
2023-04-04 01:30:39 +0530

    Added new classes and methods for Arrays1.

--- src/CodingBat/Arrays1/BiggerTwo.java
+++ src/CodingBat/Arrays1/BiggerTwo.java
@@ -1,7 +1,33 @@
 package CodingBat.Arrays1;
 
+import java.util.Arrays;
+
 public class BiggerTwo {
     public static void main(String[] args) {
-        
+        int[][] num = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println("Sum3 result Logic is::" + Arrays.toString(sum3(num)));
+
+    }
+
+    private static int[] sum3(int[][] num) {
+        int sum = 0, val = 0;
+        if (num[0].length > 0) {
+            for (int j : num[0]) {
+                sum += j;
+            }
+        }
+        if (num[1].length > 0) {
+            for (int j : num[1]) {
+                val += j;
+            }
+        }
+        if (sum == val) {
+            return num[0];
+        } else if (sum > val) {
+            return num[0];
+        } else {
+            return num[1];
+        }
     }
 }
+
