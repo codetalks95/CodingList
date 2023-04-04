ab98ade4cc3c706d29278fe46300699e23d876bf
CodeTalks <info@codetalksdna.com>
2023-04-05 01:11:10 +0530

    Added new methods for Arrays2.

--- src/CodingBat/Arrays2/WithoutTen.java
+++ src/CodingBat/Arrays2/WithoutTen.java
@@ -1,7 +1,28 @@
 package CodingBat.Arrays2;
 
-public class WithoutTen{
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class WithoutTen {
+    public static void main(String[] args) {
+        Integer[] num = {1, 10, 10, 2};
+        System.out.println("WithoutTen result Logic is::" + " " + Arrays.toString(withoutTenLogic(num)));
+    }
+
+    public static Integer[] withoutTenLogic(Integer[] num) {
+        int i = 0;
+        while (i < num.length && num[i] != 10) {
+            i++;
+        }
+        for (int j = i + 1; j < num.length; j++) {
+            if (num[j] != 10) {
+                num[i] = num[j];
+                num[j] = 10;
+                i++;
+            }
+        }
+        for (; i < num.length; i++) {
+            num[i] = 0;
+        }
+        return num;
     }
 }
