3b8d57daba6fdf2702437df667f3238e9353146a
CodeTalks <info@codetalksdna.com>
2023-04-04 23:09:06 +0530

    Added new methods for Arrays2.

--- src/CodingBat/Arrays2/FizBuzz.java
+++ src/CodingBat/Arrays2/FizBuzz.java
@@ -1,8 +1,26 @@
 package CodingBat.Arrays2;
 
+import java.util.Arrays;
 
-public class FizBuzz{
+public class FizBuzz {
     public static void main(String[] args) {
+        int[] num = {1, 6};
+        System.out.println("FizzBuzz logic result is::" + " " + Arrays.toString(fizzBuzzLogic(num[0], num[1])));
+    }
 
+    public static String[] fizzBuzzLogic(int start, int end) {
+        String[] arr = new String[end - start];
+        for (int i = start; i < end; i++) {
+            if (i % 15 == 0) {
+                arr[i - start] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                arr[i - start] = "Fizz";
+            } else if (i % 5 == 0) {
+                arr[i - start] = "Buzz";
+            } else {
+                arr[i - start] = String.valueOf(i);
+            }
+        }
+        return arr;
     }
 }
--- src/CodingBat/Arrays2/FizzArray.java
+++ src/CodingBat/Arrays2/FizzArray.java
@@ -1,8 +1,19 @@
 package CodingBat.Arrays2;
 
 
-public class FizzArray{
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class FizzArray {
+    public static void main(String[] args) {
+        int[] num = {1, 6};
+        System.out.println("FizzArray logic result is::" + " " + Arrays.toString(fizzBuzzLogic(num)));
+    }
+
+    public static int[] fizzBuzzLogic(int[] n) {
+        int[] arr = new int[n.length];
+        for (int i = 0; i < n.length; i++) {
+            arr[i] = i;
+        }
+        return arr;
     }
 }
