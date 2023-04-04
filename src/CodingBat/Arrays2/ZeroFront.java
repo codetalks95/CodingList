c280d4e69c1d04831e2c2d87be00e0133cc4d2d8
CodeTalks <info@codetalksdna.com>
2023-04-05 01:07:11 +0530

    Added new methods for Arrays2.

--- src/CodingBat/Arrays2/ZeroFront.java
+++ src/CodingBat/Arrays2/ZeroFront.java
@@ -1,7 +1,27 @@
 package CodingBat.Arrays2;
 
-public class ZeroFront{
+public class ZeroFront {
     public static void main(String[] args) {
+        Integer[] num = {1, 0, 0, 1};
+        System.out.println("ZeroFront result Logic is::" + zeroFrontLogic(num));
+    }
 
+    public static Integer[] zeroFrontLogic(Integer[] nums) {
+        if (nums.length == 0) {
+            return nums;
+        }
+        int i = 0;
+        while (nums[i] == 0) {
+            i++;
+        }
+        for (int j = i + 1; j < nums.length; j++) {
+            if (nums[j] == 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+        return nums;
     }
 }
--- src/CodingBat/Arrays2/ZeroMax.java
+++ src/CodingBat/Arrays2/ZeroMax.java
@@ -1,7 +1,21 @@
 package CodingBat.Arrays2;
 
-public class ZeroMax{
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class ZeroMax {
+    public static void main(String[] args) {
+        Integer[] num = {0, 5, 0, 3};
+        System.out.println("ZeroMax result Logic is::" + Arrays.toString(ZeroMaxLogic(num)));
+    }
+
+    public static Integer[] ZeroMaxLogic(Integer[] num) {
+        int max = 0;
+        for (int i = num.length - 1; i >= 0; i--) {
+            if (num[i] % 2 != 0)
+                max = Math.max(max, num[i]);
+            if (num[i] == 0)
+                num[i] = max;
+        }
+        return num;
     }
 }
