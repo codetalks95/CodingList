fa0e5212ce050baa07a0dcb61d96f420c2a890ba
CodeTalks <info@codetalksdna.com>
2023-04-04 09:55:41 +0530

    Added new classes and methods for Arrays1.

--- src/CodingBat/Arrays1/No23.java
+++ src/CodingBat/Arrays1/No23.java
@@ -1,7 +1,24 @@
 package CodingBat.Arrays1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class No23 {
     public static void main(String[] args) {
-        
+        int[] num = {4, 5};
+        System.out.println("PlusTwo Logic result Logic is::" + no23Logic(num));
+
+    }
+
+    private static boolean no23Logic(int[] num) {
+        List<Boolean> booleanList = new ArrayList<>();
+        for (int j : num) {
+            if (j == 2 || j == 3) {
+                booleanList.add(false);
+            } else {
+                booleanList.add(true);
+            }
+        }
+        return !booleanList.contains(false);
     }
 }
