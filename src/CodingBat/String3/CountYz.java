80cba9f96f9ba48fd169e0680d2a5604b27746f3
CodeTalks <info@codetalksdna.com>
2023-04-09 21:09:34 +0530

    Completed all code for Strings3

--- src/CodingBat/String3/CountYz.java
+++ src/CodingBat/String3/CountYz.java
@@ -6,8 +6,10 @@ public class CountYz {
     }
 
     public static int countYZLogic(String str) {
-        if (str.length() == 0) return 0;
         int count = 0;
+        if (str.length() == 0) {
+            return 0;
+        }
         for (int i = 0; i <= str.length() - 2; i++) {
             if ((Character.toLowerCase(str.charAt(i)) == 'y' || Character.toLowerCase(str.charAt(i)) == 'z') && !Character.isLetter(str.charAt(i + 1)))
                 count++;
